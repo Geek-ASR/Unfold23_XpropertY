d8c860b1b6edfc35f3facd7d63bdf71bd9e594c3
Nikhil Sunil Waghmode <nick13waghmode@gmail.com>
2023-10-21 11:32:17 +0530

    Add files via upload

---
+++ src/metadata/one.js
@@ -0,0 +1,204 @@
+const hello = [
+    {
+     "name": "DS MAX Sukruth",
+     "address": "157 W 57th St APT 49B, chandapura, Bengaluru",
+     "description": "Luxury Penthouse located in the heart of Bengaluru",
+     "image": "https://tse4.mm.bing.net/th?id=OIP.Ydb3sOsFxbtqCiUJn4La3AHaEK&pid=Api&P=0&h=180",
+     "id": "1",
+     "attributes": [
+         {
+             "trait_type": "Purchase Price",
+             "value": 20
+         },
+         {
+             "trait_type": "Type of Residence",
+             "value": "Condo"
+         },
+         {
+             "trait_type": "Bed Rooms",
+             "value": 2
+         },
+         {
+             "trait_type": "Bathrooms",
+             "value": 3
+         },
+         {
+             "trait_type": "Square Feet",
+             "value": 2200
+         },
+         {
+             "trait_type": "Year Built",
+             "value": 2013
+         }
+     ]
+ },
+ {
+    "name": "Sri Balaji Serene",
+    "address": "70780 Tamarisk Ln, Yelahanka, Bengaluru",
+    "description": "Beautiful modern home in Yelahanka",
+    "image": "https://tse2.mm.bing.net/th?id=OIP.YYVhSYmzLvBEzW_t3coXSAHaEN&pid=Api&P=0&h=180",
+    "id": "2",
+    "attributes": [
+        {
+            "trait_type": "Purchase Price",
+            "value": 15
+        },
+        {
+            "trait_type": "Type of Residence",
+            "value": "Single family residence"
+        },
+        {
+            "trait_type": "Bed Rooms",
+            "value": 4
+        },
+        {
+            "trait_type": "Bathrooms",
+            "value": 4
+        },
+        {
+            "trait_type": "Square Feet",
+            "value": 3979
+        },
+        {
+            "trait_type": "Year Built",
+            "value": 1980
+        }
+    ]
+ },
+ {
+    "name": "Mahaveer Grandis",
+    "address": "183 Woodland Drive, Jp nagar phase 7, Bengaluru",
+    "description": "Comfy island home on Camano Island",
+    "image": "https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/3.png",
+    "id": "3",
+    "attributes": [
+        {
+            "trait_type": "Purchase Price",
+            "value": 10
+        },
+        {
+            "trait_type": "Type of Residence",
+            "value": "Single family residence"
+        },
+        {
+            "trait_type": "Bed Rooms",
+            "value": 3
+        },
+        {
+            "trait_type": "Bathrooms",
+            "value": 3
+        },
+        {
+            "trait_type": "Square Feet",
+            "value": 2202
+        },
+        {
+            "trait_type": "Year Built",
+            "value": 2020
+        }
+    ]
+ },
+
+ {
+    "name": "Terra Alegria",
+    "address": "698, wof street, Whitefield",
+    "description": "Comfy island home on Camano Island",
+    "image": "https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "id": "3",
+    "attributes": [
+        {
+            "trait_type": "Purchase Price",
+            "value": 10
+        },
+        {
+            "trait_type": "Type of Residence",
+            "value": "Single family residence"
+        },
+        {
+            "trait_type": "Bed Rooms",
+            "value": 3
+        },
+        {
+            "trait_type": "Bathrooms",
+            "value": 3
+        },
+        {
+            "trait_type": "Square Feet",
+            "value": 2202
+        },
+        {
+            "trait_type": "Year Built",
+            "value": 2020
+        }
+    ]
+ },
+
+ {
+    "name": "WA Nature Home",
+    "address": "Uttarahalli, Bengaluru",
+    "description": "Comfy island home on Camano Island",
+    "image": "https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/3.png",
+    "id": "3",
+    "attributes": [
+        {
+            "trait_type": "Purchase Price",
+            "value": 10
+        },
+        {
+            "trait_type": "Type of Residence",
+            "value": "Single family residence"
+        },
+        {
+            "trait_type": "Bed Rooms",
+            "value": 3
+        },
+        {
+            "trait_type": "Bathrooms",
+            "value": 3
+        },
+        {
+            "trait_type": "Square Feet",
+            "value": 2202
+        },
+        {
+            "trait_type": "Year Built",
+            "value": 2020
+        }
+    ]
+ },
+ {
+    "name": "WA shivganaga   Home",
+    "address": "Sarjapur Road, Bengaluru",
+    "description": "Comfy island home on Camano Island",
+    "image": "https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/3.png",
+    "id": "3",
+    "attributes": [
+        {
+            "trait_type": "Purchase Price",
+            "value": 10
+        },
+        {
+            "trait_type": "Type of Residence",
+            "value": "Single family residence"
+        },
+        {
+            "trait_type": "Bed Rooms",
+            "value": 3
+        },
+        {
+            "trait_type": "Bathrooms",
+            "value": 3
+        },
+        {
+            "trait_type": "Square Feet",
+            "value": 2202
+        },
+        {
+            "trait_type": "Year Built",
+            "value": 2020
+        }
+    ]
+ }
+ ]
+ 
+ export default hello
